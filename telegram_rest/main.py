0cd65ba12c1142896e4aee9dc69a0233e10ebd67
Malte Sahrhage <malte.sahrhage@stud.uni-goettingen.de>
2025-03-03 20:10:45 +0100

    Implement Resend by Reaction

--- telegram_rest/main.py
+++ telegram_rest/main.py
@@ -2,9 +2,14 @@ from flask import Flask, request, jsonify
 from telethon import TelegramClient
 import asyncio
 import os
+import pytz
 from dotenv import load_dotenv
 import sqlite3
 import json
+import base64
+import re
+from datetime import datetime
+
 
 # Load environment variables
 load_dotenv()
@@ -34,7 +39,7 @@ asyncio.set_event_loop(loop)
 client = TelegramClient(SESSION_FILE, API_ID, API_HASH)
 
 
-async def send_message(message):
+async def send_log(message):
     async with client:
         try:
             dialogs = await client.get_dialogs()
@@ -49,6 +54,67 @@ async def send_message(message):
         except ValueError as e:
             print(f"Error: {e}")
 
+async def send_message_func(chat_id,message):
+    async with client:
+        try:
+            dialogs = await client.get_dialogs()
+            # Print chats
+            for chat in dialogs:
+                if chat.id==chat_id:
+                    print("Found")
+            entity = await client.get_entity(chat_id)  # Force fetching the entity
+            if not message["media"]:
+                message = await client.send_message(entity, str(message["message"]))
+            else:
+                image_path = "tmp_media.jpg"
+                base64_to_image(message["media"]["base64"], image_path)
+                message = await client.send_file(entity, image_path,caption = message["message"])
+            print(f"Message sent successfully! ({message.id} in {message.chat_id})")
+        except ValueError as e:
+            print(f"Error: {e}")
+
+def file_to_base64(file_path):
+    """Convert a file to a base64-encoded string."""
+    with open(file_path, "rb") as f:
+        return base64.b64encode(f.read()).decode("utf-8")
+
+def base64_to_image(base64_string, output_filename):
+    """Converts a Base64 string to an image file."""
+    try:
+        image_data = base64.b64decode(base64_string)  # Decode Base64 to binary
+        with open(output_filename, "wb") as file:
+            file.write(image_data)
+        print(f"Image saved as {output_filename}")
+        return True
+    except Exception as e:
+        print(f"Image save failed for {output_filename}")
+        return False
+def check_date(message):
+    date = message.date
+    
+
+
+    # Extract and convert timestamp
+    utc_time = datetime.strptime(date.isoformat(), "%Y-%m-%dT%H:%M:%S+00:00")
+
+    utc = pytz.utc
+    berlin_tz = pytz.timezone('Europe/Berlin')
+    utc_time = utc.localize(utc_time)
+    date = utc_time.astimezone(berlin_tz)
+    
+
+    if message.message:
+        msg_text = message.message
+        if re.search("Date",msg_text):
+            lines = msg_text.splitlines()
+            for line in lines:
+                if re.search("^Date",line):
+                    date_string=line
+                    date_string = re.sub("^Date:","",date_string).strip()
+                    date_info = date_string.split("-")
+                    date = date.replace(year=int(date_info[0]),month=int(date_info[1]),day=int(date_info[2]))
+    date = date.strftime('%Y-%m-%dT%H:%M:%S')    
+    return(date)
 
 async def get_message_func(channel_id,message_id):
     async with client:
@@ -61,15 +127,40 @@ async def get_message_func(channel_id,message_id):
                     print("Hallo")
             entity = await client.get_entity(channel_id)  # Force fetching the entity
             message = await client.get_messages(entity, ids=message_id)
-            print(f"Message received successfully! ({message.id} in {message.chat_id})")
+            if(message):
+                print(f"Message received successfully! ({message.id} in {message.chat_id})")
 
             if message:
-                print(f"Message ID: {message.id}")
-                print(f"Date: {message.date}")
-                print(f"Text: {message.message}")
-                print(f"Sender ID: {message.sender_id}")
+                msg_dict = {
+                    "id": message.id,
+                    "date_original": message.date.isoformat(),
+                    "date": check_date(message),
+                    "message": message.message,
+                    "out": message.out,
+                    "mentioned": message.mentioned,
+                    "media": None,  # Convert media to string or handle separately
+                    "sender_id": message.from_id.user_id if message.from_id else None,
+                    "chat_id": message.peer_id.channel_id if message.peer_id else None,
+                    "reply_to": message.reply_to.reply_to_message_id if message.reply_to else None
+                }
 
-                return {"message_id": message.id, "date": message.date.isoformat(), "text": message.message}
+                # Check if the message contains media
+                if message.media:
+                    media_path = await message.download_media(file="temp_media.jpg")
+                    
+                    # Convert to Base64
+                    media_base64 = file_to_base64(media_path)
+                    base64_to_image(media_base64, "output_image.jpg")                    
+                    # Store media as Base64 in JSON
+                    msg_dict["media"] = {
+                        "type": type(message.media).__name__,  # Get media type (Photo, Document, etc.)
+                        "media_string": str(message.media),
+                        "base64": media_base64
+                    }             
+
+
+                json_msg = msg_dict
+                return json_msg
             else:
                 print("Message not found.")
         except ValueError as e:
@@ -118,6 +209,20 @@ async def get_chat_id_from_name_func(channel_name):
         except ValueError as e:
             print(f"Error: {e}")
 
+async def delete_message_func(channel_id,message_id):
+    async with client:
+        try:
+            dialogs = await client.get_dialogs()
+            # Print chats
+            for chat in dialogs:
+                if chat.id==channel_id:
+                    channel_id = chat.id
+            entity = await client.get_entity(channel_id)  # Force fetching the entity
+            await client.delete_messages(entity, message_id)
+            print(f"Message deleted successfully! ({message_id} in {channel_id})")
+        except ValueError as e:
+            print(f"Error: {e}")
+
 
 
 @app.route('/log', methods=['POST'])
@@ -130,7 +235,7 @@ def receive_log():
 
     log_message = data['message']
 
-    loop.run_until_complete(send_message(log_message))
+    loop.run_until_complete(send_log(log_message))
 
     return jsonify({"status": "Message sent"}), 200
 
@@ -150,11 +255,31 @@ def get_message():
 
     message = loop.run_until_complete(get_message_func(channel_id,message_id))
 
-    return jsonify({"status": "Message received", "message": message}), 200
+    return jsonify({"status": "Message received", "time_stamp": datetime.now(pytz.timezone('Europe/Berlin')).isoformat(),"message": message}), 200
+
+@app.route('/delete_message', methods=['POST'])
+def delete_message():
+    """API endpoint to receive logs and forward to Telegram."""
+    data = request.get_json()
+    
+    if not data or 'chat_id' not in data or 'message_id' not in data:
+        return jsonify({"error": "Invalid request"}), 400
+
+    channel_id = int(data['chat_id'])
+    message_id = int(data['message_id'])
+    print(channel_id)
+    print(message_id)
+
+    loop.run_until_complete(delete_message_func(channel_id,message_id))
+
+    return jsonify({"status": "Message deleted", "time_stamp": datetime.now(pytz.timezone('Europe/Berlin')).isoformat()}), 200
+
 
 @app.route('/get_messages_reaction', methods=['POST'])
 def get_messages_reaction():
-    """API endpoint to receive logs and forward to Telegram."""
+    """API endpoint to receive logs and forward to Telegram.
+    POST input: chat_id (int)
+    """
     data = request.get_json()
     
     if not data or 'chat_id' not in data:
@@ -164,6 +289,7 @@ def get_messages_reaction():
     print(channel_id)
 
     message_ids = loop.run_until_complete(get_messages_from_reaction(channel_id, "👍"))
+    #message_ids = loop.run_until_complete(get_messages_from_reaction(channel_id, "🔥"))
 
     return jsonify({"status": "Messages found", "message_ids": message_ids}), 200
 
@@ -184,6 +310,26 @@ def get_chat_id_from_name():
     else:
         return jsonify({"status": "ID not found", "chat_id": None}), 200
 
+
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    """API endpoint to receive logs and forward to Telegram."""
+    data = request.get_json()
+    
+    if not data or 'chat_id' not in data or 'message' not in data:
+        return jsonify({"error": "Invalid request"}), 400
+
+    channel_id = int(data['chat_id'])
+    message = json.loads(data['message'])
+    message_text = message['message']['message']
+    message_id = message['message']['id']
+    message["message"]["message"] = f"{message_text}\nMID: {message_id}"
+    print(channel_id)
+
+
+    message = loop.run_until_complete(send_message_func(channel_id,message["message"]))
+    return jsonify({"status": "Message sent", "time_stamp": datetime.now(pytz.timezone('Europe/Berlin')).isoformat(),"message": message}), 200
 
 
 if __name__ == '__main__':
--- tests/test_save_diary_messages.py
+++ tests/test_save_diary_messages.py
@@ -1,14 +1,33 @@
 import pytest
+import boto3
 from telegram_rest.main import app
 import json
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+
 
 # Load environment variables
 load_dotenv()
-
+CHANNEL_ID=os.getenv('CHANNEL_ID') 
 DIARY_CHANNEL=os.getenv('DIARY_CHANNEL') 
 OUTPUT_DIR = os.getenv('OUTPUT_DIR')
+
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT')
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+
+# Connect to MinIO
+s3 = boto3.client(
+    "s3",
+    endpoint_url=MINIO_ENDPOINT,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+)
+
+
 
 @pytest.fixture
 def client():
@@ -18,13 +37,56 @@ def test_get_messages_from_reaction(client):
     response = client.post('/get_messages_reaction', json={"chat_id": DIARY_CHANNEL})
     response_json = response.get_json()
     assert response.status_code == 200
-    reaction_ids = response_json["message_ids"]
-    print(f"{reaction_ids} ({len(reaction_ids)})")
+    reaction_ids_api = response_json["message_ids"]
+    print(f"{reaction_ids_api} ({len(reaction_ids_api)})")
+
+
+    response = client.post('/get_messages_reaction', json={"chat_id": CHANNEL_ID})
+    response_json = response.get_json()
+    assert response.status_code == 200
+    reaction_ids_logs = response_json["message_ids"]
+    print(f"{reaction_ids_logs} ({len(reaction_ids_logs)})")
+
+    reaction_ids = [] 
+    reaction_ids.append(reaction_ids_api)
+    reaction_ids.append(reaction_ids_logs)
+    reaction_ids = sum(reaction_ids, [])
+    print(f"REACTION IDs: {reaction_ids}")
+    
 
     for reaction_id in reaction_ids:
-        response = client.post('/get_message', json={"chat_id": DIARY_CHANNEL, "message_id": reaction_id})
-        assert response.status_code == 200
+        if(reaction_id in reaction_ids_api):
+            channel = DIARY_CHANNEL
+        else:
+            channel = CHANNEL_ID
 
-        with open(os.path.join(OUTPUT_DIR,f"{reaction_id}.json"), 'w', encoding='utf-8') as f:
-            json.dump(response.get_json()["message"], f, ensure_ascii=False, indent=4)
+        response = client.post('/get_message', json={"chat_id": channel, "message_id": reaction_id})
+        assert response.status_code == 200
+        message = response.get_json()
+
+
+
+        time_stamp = message['message']['date']
+        time_stamp = datetime.strptime(time_stamp,'%Y-%m-%dT%H:%M:%S')
+        # Get current timestamp for folder structure
+        year, month, day, hour = time_stamp.strftime("%Y"), time_stamp.strftime("%m"), time_stamp.strftime("%d"), time_stamp.strftime("%H")
+        minio_path = f"messages/raw/year={year}/month={month}/day={day}/{message["message"]["chat_id"]}_{message["message"]["id"]}.json"
+
+
+        # with open(tmp.name, 'w', encoding='utf-8') as f:
+        #     json.dump(message, f, ensure_ascii=False, indent=2)
+
+        # with open(tmp.name, "rb") as file_data:
+        s3.put_object(Bucket=BUCKET_NAME, Key=minio_path, Body=json.dumps(message,indent=2), ContentType="application/json")
+        print(f"Uploaded: → {minio_path}")
+        if message["message"]["id"]:
+            response = client.post('/delete_message', json={"chat_id": DIARY_CHANNEL, "message_id": reaction_id})
+            response_json = response.get_json()
+            assert response.status_code == 200
+            print(f"Deleted: → {message["message"]["id"]}")
+            
+            response = client.post('/send_message', json={"chat_id": CHANNEL_ID, "message": json.dumps(message)})
+            response_json = response.get_json()
+            assert response.status_code == 200
+            print(f"SENT: → {message["message"]["id"]}")
 
